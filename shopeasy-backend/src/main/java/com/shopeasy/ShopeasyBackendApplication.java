07af83cd0cff99d8aeade9e99c1ce4b415a7f3b1
Sanchit Dhiman <sanchitdhiman04@gmail.com>
2025-09-19 23:18:26 +0530

    Initialized Project

---
+++ shopeasy-backend/src/main/java/com/shopeasy/ShopeasyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeasyBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ShopeasyBackendApplication.class, args);
+    }
+    
+}
---
+++ shopeasy-backend/src/test/java/com/shopeasy/ShopeasyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeasyBackendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
